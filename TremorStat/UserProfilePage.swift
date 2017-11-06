9fa49a1232ad540d54a448a240adf0b084a12155
ikukushk <ikukushk@asb9700-e01.csil.sfu.ca>
2017-11-05 19:42:30 -0800

    Added a table view controller with a picker view that can be hidden

---
+++ TremorStat/UserProfilePage.swift
@@ -0,0 +1,134 @@
+//
+//  UserProfilePage.swift
+//  TremorStat
+//
+//  Created by ikukushk on 11/5/17.
+//  Copyright © 2017 Best Software. All rights reserved.
+//
+
+import UIKit
+
+class UserProfilePage: UITableViewController {
+
+    var datePickerHidden = false
+
+    @IBOutlet weak var dateOfBirthLabel: UILabel!
+    
+    @IBOutlet weak var dateOfBirthPicker: UIDatePicker!
+    
+    @IBAction func datePickerValue(sender: UIDatePicker) {
+        datePickerChanged()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        datePickerChanged()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            toggleDatePicker()
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if datePickerHidden && indexPath.section == 0 && indexPath.row == 1 {
+            return 0
+        }
+        else {
+            return super.tableView(self.tableView, heightForRowAt: indexPath)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func datePickerChanged () {
+        dateOfBirthLabel.text = DateFormatter.localizedString(from: dateOfBirthPicker.date, dateStyle: DateFormatter.Style.short, timeStyle: DateFormatter.Style.short)
+    }
+    
+    func toggleDatePicker() {
+        datePickerHidden = !datePickerHidden
+        
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+    
+    // MARK: - Table view data source
+
+    /*override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }*/
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
