75c6a6a40ed61d7b47f8d432d6f3661b62f214b6
Domenico Di Giovanni <ddigiova@wmc2502-mac14.ucslabs.sfu.ca>
2017-11-17 16:12:39 -0800

    added lots of comments to the restTremorConclusion page

--- TremorStat/TestResults/RestTremorConclusion.swift
+++ TremorStat/TestResults/RestTremorConclusion.swift
@@ -17,9 +17,13 @@
 
 import UIKit
 
+// amount of tests user must complete before conclusion will be calculated
 let ACCEPTABLE_AMOUNT_OF_TESTS = 2
 
+// based on our results ( all our averages added together with all our standad deviations ) + little extra leeway ( 0.125 --> 0.15 )
 let AVG_PERSON_PLUS_STDDEV = 0.15
+
+// double above value to catch large shakes
 let AVG_PERSON_PLUS_TWO_STDDEV = 0.3
 
 
@@ -36,6 +40,7 @@ class RestTremorConclusion: UIViewController {
     @IBOutlet weak var ConclusionLabel: UILabel!
     
     
+    // if the user wishes to know how they're doing recently, this button will cause recalculations based on only recent results
     @IBAction func recentTestsOnly(_ sender: Any) {
         displayInfo( displayRecentInfoOnly: true )
     }
@@ -46,11 +51,11 @@ class RestTremorConclusion: UIViewController {
         
     }
 
+    // display info to user
     override func viewDidAppear(_ animated: Bool) {
 
-    
+        // display info to the user ( lifetime results )
         displayInfo( displayRecentInfoOnly: false )
-        // Do any additional setup after loading the view.
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -59,9 +64,14 @@ class RestTremorConclusion: UIViewController {
     
     // MARK: Implementation
     
+    // display info for user ( Summary of user profile ( bools ) -- Indication of completed amount of tests -- Conclusion )
     func displayInfo( displayRecentInfoOnly: Bool) {
         // SET UP SUMMARY PAGE
+        
+        // string that will be modular for the user profile summary
         var summaryString = " "
+        
+        // count used to see how many fields the user checks out for
         var stringCount = 0
         
         // construct string based on user profile
@@ -82,7 +92,7 @@ class RestTremorConclusion: UIViewController {
             stringCount = stringCount + 1
         }
         
-        // print appropriate response as to why the previous data was displayed
+        // print appropriate response as to why the previous data was displayed -- have gramatically correct case for singular factor
         if stringCount > 2 {
             summaryString = summaryString + " Each of these factors increase your chance of getting Parkinson's. "
         }
@@ -96,8 +106,11 @@ class RestTremorConclusion: UIViewController {
         // for displaying to the user whether or not they have enough data to draw a conclusion from
         var insufficientDataString: String!
         insufficientDataString = " "
+        
+        // default the labels to blanks values
         self.InsufficientDataLabel.text = insufficientDataString
         self.ConclusionLabel.text = " "
+        
         // if the amount of tests is less than the threshold - prevent user from seeing the data
         if ( restTremorResultArray.count < ACCEPTABLE_AMOUNT_OF_TESTS ) {
             
@@ -108,51 +121,69 @@ class RestTremorConclusion: UIViewController {
         else {
             
             // conclusion
+            
+            // display amount of completed tests
             insufficientDataString = "You have completed " + String( restTremorResultArray.count ) + " tests."
             self.InsufficientDataLabel.text = insufficientDataString
-            //var statCalc = StatisticsCalculator()
+            
+            // used to calculate the users average shakiness
             var avgStat = 0.0
             
-            print( " array size is ", restTremorResultArray.count )
+            // only run rest of calculations if tests have been completed
             if restTremorResultArray.count != 0 {
+                
+                // for singular test completed
                 if restTremorResultArray.count == 1 {
                     avgStat = restTremorResultArray[0].testAverageX + restTremorResultArray[0].testAverageY + restTremorResultArray[0].testAverageZ
                 }
                 else {
+                    
+                    // the start index for the calculation
                     var startIndex = 0
+                    
+                    // if the user just wants to see recent data, change startIndex so it only accounts for new results
                     if ( displayRecentInfoOnly == true ) {
                         startIndex = restTremorResultArray.count - 1 - ACCEPTABLE_AMOUNT_OF_TESTS
+                        
+                        // catch negative cases
                         if( startIndex < 0 ) {
                             startIndex = 0
                         }
                     }
+                    
+                    // either go thru all tests or recent tests
                     for i in startIndex...(restTremorResultArray.count - 1) {
                         
+                        // increment average based on averages of each test
                         avgStat += restTremorResultArray[i].testAverageX + restTremorResultArray[i].testAverageY + restTremorResultArray[i].testAverageZ
                         print ( " avgstat is ", avgStat )
-                        //stdDevStat += restTremorResultArray[i].testStdDevX + restTremorResultArray[i].testStdDevY + restTremorResultArray[i].testStdDevZ
-                        //print ( " stddevstat is ", stdDevStat )
                     }
                     
                 }
                 
+                // take average
                 avgStat = avgStat/Double( restTremorResultArray.count )
-                print( "final avgStat is " , avgStat )
-                //stdDevStat = stdDevStat/Double( restTremorResultArray.count )
-                // print ( " final stddevstat is ", stdDevStat )
                 
+                // declare conclusionString to display results to the user
                 var conclusionString: String!
                 
+                // if the users average was higher than our baseline plus our standard deviation --> they are higher than average but not drastically
                 if ( avgStat > AVG_PERSON_PLUS_TWO_STDDEV ) {
                     
+                    // string for if they do not have many factors
                     conclusionString = "You shake statistically higher than the average person. You do not have many contributing factors, but you should still consult a doctor for proper diagnosis. "
+                    
+                    // if the user does have a lot of additional factors in their profile, this higher than average value might not be a fluke...
                     if ( stringCount > 1 ) {
                         conclusionString = "You shake statistically higher than the average person. Additionally you have some factors that increase your risk. You should consult a doctor for proper diagnosis. "
                     }
                     
                 }
+                    
+                // now if the user's average was higher than our average plus twice our standard deviation --> they are well above averages
                 else if ( avgStat > AVG_PERSON_PLUS_STDDEV ) {
                     
+                    // regardless of user profile we will recommend diagnosis at this point, but change message regardless
                     conclusionString = "You do shake more than the average person. You do not have many contributing factors though. The results are inconclusive."
                     if ( stringCount > 1 ) {
                         conclusionString = "You do shake more than the average person. As you do have some contributing factors there is a possibility that you have Parkinsons's. Consult a doctor for proper diagnosis. "
@@ -160,16 +191,19 @@ class RestTremorConclusion: UIViewController {
                     
                 }
                 else {
+                    // user's average was within "average" range
                     conclusionString = "You are within the average ranges of shakiness that someone without Parkinson's has. So far you are fine. "
                 }
                 
+                // update label text based on calculations above
                 ConclusionLabel.text = conclusionString
                 
             }
             else {
+                
+                // user has not done any tests yet
                 ConclusionLabel.text = "You do not have any test data yet."
             }
-            
             
         }
     }
