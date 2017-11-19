827f72e0b847dd7314c9ddcc2da4d35ba1e34040
Milos Savic <msavic@asb9700-f01.csil.sfu.ca>
2017-11-18 18:41:28 -0800

    FIXED THE DAMN BUTTONS AGAIN

---
+++ TremorStat/TestResults/TestResultsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TestResultsViewController.swift
+//  TremorStat
+//
+//  Created by Milos Savic on 11/18/17.
+//  Copyright © 2017 Best Software. All rights reserved.
+//
+
+import UIKit
+
+class TestResultsViewController: UIViewController {
+    
+    @IBOutlet weak var ActiveTremorTestResultsButton: UIButton!
+    @IBOutlet weak var RestTremorTestResultsButton: UIButton!
+    @IBOutlet weak var TestResultsAnalysisButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        ActiveTremorTestResultsButton.layer.cornerRadius = 25
+        RestTremorTestResultsButton.layer.cornerRadius = 25
+        TestResultsAnalysisButton.layer.cornerRadius = 25
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
