d32ca4f671aa66d99efb2ef529ffffad74ce8d9b
Milos Savic <msavic@asb9700-f01.csil.sfu.ca>
2017-11-18 16:29:04 -0800

    Saved user profile data to the device as a default parameter.

--- TremorStat/UserProfile/UserProfileViewController.swift
+++ TremorStat/UserProfile/UserProfileViewController.swift
@@ -20,17 +20,27 @@ class UserProfileViewController: FormViewController {
             <<< DateRow() {
                 $0.value = Date()
                 $0.title = "Date of Birth"
+                if let UserDOB = UserDefaults.standard.object(forKey: "UserDOB") as? Date
+                {
+                    $0.value = UserDOB
+                }
             }
             .onChange { row in
                 userProfileResults.dateOfBirth = row.value!
+                UserDefaults.standard.set(userProfileResults.dateOfBirth, forKey: "UserDOB")
             }
             <<< ActionSheetRow<String>() {
                 $0.title = "Gender"
                 $0.selectorTitle = "What is your gender?"
                 $0.options = ["Male", "Female"]
+                    if let UserGender = UserDefaults.standard.object(forKey: "UserGender") as? String
+                    {
+                        $0.value = UserGender
+                    }
                 }
                 .onChange { row in
                     userProfileResults.gender = row.value!
+                    UserDefaults.standard.set(userProfileResults.gender, forKey: "UserGender")
                 }
                 .onPresent { from, to in
                     to.popoverPresentationController?.permittedArrowDirections = .up
@@ -39,9 +49,14 @@ class UserProfileViewController: FormViewController {
                 $0.title = "Genetics"
                 $0.selectorTitle = "Does your biological family have a history of Parkinson's disease?"
                 $0.options = ["Yes", "No", "Unsure"]
+                    if let UserGenetics = UserDefaults.standard.object(forKey: "UserGenetics") as? String
+                    {
+                        $0.value = UserGenetics;
+                    }
                 }
                 .onChange { row in
                     userProfileResults.genetics = row.value!
+                    UserDefaults.standard.set(userProfileResults.genetics, forKey: "UserGenetics");
                 }
                 .onPresent { from, to in
                     to.popoverPresentationController?.permittedArrowDirections = .up
@@ -50,9 +65,14 @@ class UserProfileViewController: FormViewController {
                 $0.title = "Cervical Trauma"
                 $0.selectorTitle = "Have you experienced any neck damage throughout your lifetime?"
                 $0.options = ["Yes", "No", "Unsure"]
+                    if let UserTrauma = UserDefaults.standard.object(forKey: "UserTrauma") as? String
+                    {
+                        $0.value = UserTrauma;
+                    }
                 }
                 .onChange { row in
                     userProfileResults.trauma = row.value!
+                    UserDefaults.standard.set(userProfileResults.trauma, forKey: "UserTrauma");
                 }
                 .onPresent { from, to in
                     to.popoverPresentationController?.permittedArrowDirections = .up
@@ -61,9 +81,14 @@ class UserProfileViewController: FormViewController {
                 $0.title = "Symptoms"
                 $0.selectorTitle = "What is the severity of your symptoms related to Parkinson's disease?"
                 $0.options = ["Not Diagnosed", "Mild", "Moderate", "Severe"]
+                    if let UserStage = UserDefaults.standard.object(forKey: "UserStage") as? String
+                    {
+                        $0.value = UserStage;
+                    }
                 }
                 .onChange { row in
                     userProfileResults.stage = row.value!
+                    UserDefaults.standard.set(userProfileResults.stage, forKey: "UserStage")
                 }
                 .onPresent { from, to in
                     to.popoverPresentationController?.permittedArrowDirections = .up
@@ -71,10 +96,17 @@ class UserProfileViewController: FormViewController {
             +++ Section("Additional Questions")
             <<< MultipleSelectorRow<String>() {
                 $0.title = "Environmental Triggers"
-                $0.options = ["Herbicides", "Pesicies"]
+                $0.options = ["Herbicides", "Pesticides"]
+                if let UserEnv = UserDefaults.standard.object(forKey: "UserEnv") as? Array<Any>
+                    {
+                        let temp_UserEnv = Set(UserEnv as! [String])
+                        $0.value = temp_UserEnv;
+                    }
                 }
                 .onChange { row in
                     userProfileResults.triggers = row.value!
+                    let dataToStore = Array(userProfileResults.triggers)
+                    UserDefaults.standard.set(dataToStore, forKey: "UserEnv")
                 }
                 .onPresent { from, to in
                     to.navigationItem.backBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: from, action: #selector(UserProfileViewController.multipleSelectorDone(_:)))
@@ -82,9 +114,16 @@ class UserProfileViewController: FormViewController {
             <<< MultipleSelectorRow<String>() {
                 $0.title = "Prescription Drugs"
                 $0.options = ["Vitamin A", "Vitamin C", "Vitamin D", "Vitamin E"]
+                    if let UserDrugs = UserDefaults.standard.object(forKey: "UserDrugs")as? Array<Any>
+                    {
+                        let temp_UserDrugs = Set(UserDrugs as! [String])
+                        $0.value = temp_UserDrugs
+                    }
                 }
                 .onChange { row in
                     userProfileResults.drugs = row.value!
+                    let dataToStore = Array(userProfileResults.drugs);
+                    UserDefaults.standard.set(dataToStore, forKey: "UserDrugs")
                 }
                 .onPresent { from, to in
                     to.navigationItem.backBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: from, action: #selector(UserProfileViewController.multipleSelectorDone(_:)))
@@ -92,9 +131,16 @@ class UserProfileViewController: FormViewController {
             <<< MultipleSelectorRow<String>() {
                 $0.title = "Supplements"
                 $0.options = ["Vitamin A", "Vitamin C", "Vitamin D", "Vitamin E"]
+                    if let UserSupp = UserDefaults.standard.object(forKey: "UserSupp") as? Array<Any>
+                    {
+                        let temp_UserSupp = Set(UserSupp as! [String])
+                        $0.value = temp_UserSupp
+                    }
                 }
                 .onChange { row in
                     userProfileResults.supplements = row.value!
+                    let dataToStore = Array(userProfileResults.supplements)
+                    UserDefaults.standard.set(dataToStore, forKey: "UserSupp")
                 }
                 .onPresent { from, to in
                     to.navigationItem.backBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: from, action: #selector(UserProfileViewController.multipleSelectorDone(_:)))
@@ -103,9 +149,14 @@ class UserProfileViewController: FormViewController {
                 $0.title = "Hand"
                 $0.selectorTitle = "What hand will you be using to perform the tests?"
                 $0.options = ["Left", "Right"]
+                    if let UserHand = UserDefaults.standard.object(forKey: "UserHand") as? String
+                    {
+                       $0.value = UserHand
+                    }
                 }
                 .onChange { row in
                     userProfileResults.hand = row.value!
+                    UserDefaults.standard.set(userProfileResults.hand, forKey: "UserHand")
                 }
                 .onPresent { from, to in
                     to.popoverPresentationController?.permittedArrowDirections = .up
