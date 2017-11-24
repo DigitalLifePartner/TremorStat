d088cc1a97860e6d7ee4556bfce74ed077978efe
Domenico Di Giovanni <ddigiova@wmc2502-mac19.ucslabs.sfu.ca>
2017-11-24 15:17:16 -0800

    test commit for graphs

---
+++ TremorStat/Settings/RestTremorUserProfile.swift
@@ -0,0 +1,35 @@
+//
+//  RestTremorUserProfile.swift
+//  TremorStat
+//
+//  Created by Domenico Di Giovanni on 11/24/17.
+//  Copyright © 2017 Best Software. All rights reserved.
+//
+
+import UIKit
+
+class RestTremorUserProfile: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
