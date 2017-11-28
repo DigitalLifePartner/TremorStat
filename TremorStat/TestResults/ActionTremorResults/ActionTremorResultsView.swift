5594ea0d2a6cdced784215c3744acc0f0f1fd33e
Milos Savic <msavic@asb9700-f01.csil.sfu.ca>
2017-11-27 20:46:38 -0800

    - Major UI changes.

---
+++ TremorStat/TestResults/ActionTremorResults/ActionTremorResultsView.swift
@@ -0,0 +1,40 @@
+//
+//  ActionTremorResultsView.swift
+//  TremorStat
+//
+//  Created by Milos Savic on 11/27/17.
+//  Copyright © 2017 Best Software. All rights reserved.
+//
+
+import UIKit
+
+class ActionTremorResultsView: UIViewController {
+
+    @IBOutlet weak var PlotAllData: UIButton!
+    @IBOutlet weak var Plot30Days: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        PlotAllData.layer.cornerRadius = 25
+        Plot30Days.layer.cornerRadius = 25
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
